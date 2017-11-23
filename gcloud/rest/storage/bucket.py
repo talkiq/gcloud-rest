fc15e488cabd8dedd6ed45a4c4d58779853c2131
Kevin James <kevin@talkiq.com>
2017-11-22 17:44:18 -0800

    feat(storage): basic Bucket.download implementation

---
+++ gcloud/rest/storage/bucket.py
@@ -0,0 +1,40 @@
+import logging
+
+import requests
+
+from gcloud.rest.auth import Token
+
+
+log = logging.getLogger(__name__)
+
+API_ROOT = 'https://www.googleapis.com/storage/v1/b'
+# TODO: storage upload
+# UPLOAD_API_ROOT = 'https://www.googleapis.com/upload/storage/v1/b'
+SCOPES = [
+    'https://www.googleapis.com/auth/devstorage.read_only',
+]
+
+
+class Bucket(object):
+    def __init__(self, project, bucket, creds=None):
+        self.project = project
+        self.bucket = bucket
+
+        self.access_token = Token(creds=creds, scopes=SCOPES)
+
+    def headers(self):
+        return {
+            'Authorization': 'Bearer {}'.format(self.access_token)
+        }
+
+    def download(self, object_name, params=None):
+        # TODO: does this need actual urlescaping?
+        object_name = object_name.replace('/', '%2F')
+        url = '{}/{}/o/{}'.format(API_ROOT, self.bucket, object_name)
+
+        resp = requests.get(url, headers=self.headers(), params=params or {})
+        resp.raise_for_status()
+        return resp.text
+
+    def download_as_string(self, object_name):
+        return self.download(object_name, params={'alt': 'media'})
---
+++ tests/unit/storage/bucket_test.py
@@ -0,0 +1,5 @@
+import gcloud.rest.storage.bucket as bucket  # pylint: disable=unused-import
+
+
+def test_importable():
+    assert True
