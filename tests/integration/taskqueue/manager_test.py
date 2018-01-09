d629c1b0489f6dfc6085d4098661025915f239c2
Kevin James <kevin@talkiq.com>
2018-01-09 12:34:14 -0800

    test(taskqueue): regression test for tasks requiring multiple renews

--- tests/integration/taskqueue/manager_test.py
+++ tests/integration/taskqueue/manager_test.py
@@ -1,11 +1,14 @@
 import json
 import os
+import time
+
+import pytest
 
 from gcloud.rest.taskqueue import encode
 from gcloud.rest.taskqueue import TaskManager
 
 
-def test_lifecycle(mocker):
+def test_lifecycle(caplog, mocker):
     project = os.environ['GCLOUD_PROJECT']
     task_queue = 'test-pull'
 
@@ -27,7 +30,43 @@ def test_lifecycle(mocker):
     # insert new ones
     for task in tasks:
         tm.tq.insert(encode(json.dumps(task)),
-                     tag=encode('gcloud-rest-manager-test'))
+                     tag=encode('gcloud-rest-manager-test-lifecycle'))
 
     tm.find_and_process_work()
     assert worker.mock_calls == [mocker.call(tasks)]
+    for record in caplog.records:
+        assert record.levelname != 'ERROR'
+
+
+@pytest.mark.slow
+def test_multiple_leases(caplog, mocker):
+    project = os.environ['GCLOUD_PROJECT']
+    task_queue = 'test-pull'
+
+    tasks = [
+        {'test_idx': 1},
+        {'test_idx': 2},
+    ]
+
+    def succeed_after_multiple_leases(ts):
+        time.sleep(10)
+        return ['ok' for _ in ts]
+
+    worker = mocker.Mock()
+    worker.side_effect = succeed_after_multiple_leases
+
+    tm = TaskManager(project, task_queue, worker, batch_size=len(tasks),
+                     lease_seconds=4)
+
+    # drain old test tasks
+    drain = tm.tq.drain()
+
+    # insert new ones
+    for task in tasks:
+        tm.tq.insert(encode(json.dumps(task)),
+                     tag=encode('gcloud-rest-manager-test-multilease'))
+
+    tm.find_and_process_work()
+    assert worker.mock_calls == [mocker.call(tasks)]
+    for record in caplog.records:
+        assert record.levelname != 'ERROR'
