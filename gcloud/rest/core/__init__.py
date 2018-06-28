f479e1807331557fdf3b6cc58bdb4997fb0343e7
Kevin James <kevin@talkiq.com>
2018-06-28 12:48:39 -0700

    feat(kms): implement kms encrypt and decrypt

--- gcloud/rest/core/__init__.py
+++ gcloud/rest/core/__init__.py
@@ -2,6 +2,8 @@ from pkg_resources import get_distribution
 __version__ = get_distribution('gcloud-rest').version
 
 from gcloud.rest.core.util import backoff
+from gcloud.rest.core.util import decode
+from gcloud.rest.core.util import encode
 
 
-__all__ = ['__version__', 'backoff']
+__all__ = ['__version__', 'backoff', 'decode', 'encode']
--- gcloud/rest/taskqueue/__init__.py
+++ gcloud/rest/taskqueue/__init__.py
@@ -4,8 +4,10 @@ __version__ = get_distribution('gcloud-rest').version
 from gcloud.rest.taskqueue.error import FailFastError
 from gcloud.rest.taskqueue.manager import TaskManager
 from gcloud.rest.taskqueue.queue import TaskQueue
-from gcloud.rest.taskqueue.utils import decode
-from gcloud.rest.taskqueue.utils import encode
+
+# TODO: deprecate
+from gcloud.rest.core.util import decode
+from gcloud.rest.core.util import encode
 
 
 __all__ = ['__version__', 'FailFastError', 'TaskManager', 'TaskQueue',
